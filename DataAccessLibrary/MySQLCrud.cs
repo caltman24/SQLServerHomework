d417903e0070a35e75dbe97a03c938f1ebee7942
Corbyn Altman <corbyn.altman01@gmail.com>
2022-10-08 20:36:19 -0400

    added Mysql connection

---
+++ DataAccessLibrary/MySQLCrud.cs
@@ -0,0 +1,36 @@
+﻿using DataAccessLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    // TODO: Fnish class
+    public class MySQLCrud : ISQLCrud
+    {
+        private readonly string _connectionString;
+        private readonly ISQLDataAccess _db = new MySQLDataAccess();
+
+        public MySQLCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void CreatePerson(PersonModel person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<PersonModel> GetAllPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PersonModel? GetPersonById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
